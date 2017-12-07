173b1a5aab4a0bf2d212def7bb6384b2cdd68fa3
MyDreGo <james@mydrego.com>
2017-12-07 21:31:05 +0800

    final

--- app/src/main/java/com/example/student/a20171207_1/MainActivity.java
+++ app/src/main/java/com/example/student/a20171207_1/MainActivity.java
@@ -2,12 +2,68 @@ package com.example.student.a20171207_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new UTF8StringRequest("https://udn.com/rssfeed/news/2/6638?ch=news",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        final MyHandler dataHandler = new MyHandler();
+                        SAXParserFactory spf = SAXParserFactory.newInstance();
+                        SAXParser sp = null;
+                        try {
+                            sp = spf.newSAXParser();
+                            XMLReader xr = sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(response)));
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                    MainActivity.this,
+                                    android.R.layout.simple_list_item_1,
+                                    dataHandler.mylist
+                            );
+                            lv.setAdapter(adapter);
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
     }
 }
